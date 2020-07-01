f379b8e244a601948e4115a0698d179d5100c787
Jose Quinones <josemanuelqn.88@gmail.com>
2020-07-01 17:15:13 +0100

    add festure tests

--- server.rb
+++ server.rb
@@ -4,6 +4,10 @@ require 'socket'
 
 PORT = '8080'
 
-TCPServer.new('localhost', PORT)
+server = TCPServer.new('localhost', PORT)
 puts "Nails server started on #{PORT}"
 puts 'To stop the server enter CTRL+C'
+
+client = server.accept
+request = client.readpartial(2048)
+puts request
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -10,3 +10,11 @@ RSpec.describe 'Run server', type: :aruba do
 
   it { expect(last_command_started).to have_output(/Nails server started on 8080/) }
 end
+
+RSpec.describe 'Visit main page', type: :feature do
+  it 'can see nails success message' do
+    visit '/'
+
+    expect(page).to have_content 'You nailed it!'
+  end
+end
