86584fe9bac361eeedd07a8db26f5f6e27ebb7fb
Jose Quinones <josemanuelqn.88@gmail.com>
2020-07-24 09:03:49 +0100

    update server file to send back a response
    The server now sends a response back to the client (new session).
    The feature test works if I have the server running.

--- server.rb
+++ server.rb
@@ -8,6 +8,14 @@ server = TCPServer.new('localhost', PORT)
 puts "Nails server started on #{PORT}"
 puts 'To stop the server enter CTRL+C'
 
-client = server.accept
-request = client.readpartial(2048)
-puts request
+while session = server.accept
+  request = session.gets
+  puts request
+
+  session.print "HTTP/1.1 200\r\n"
+  session.print "Content-Type: text/html\r\n"
+  session.print "\r\n"
+  session.print 'You nailed it!'
+
+  session.close
+end
